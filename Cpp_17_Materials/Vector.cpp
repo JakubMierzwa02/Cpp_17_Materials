52dede198fab89b81f28e65da3d5a53310f5af14
weltix <102422732+JakubMierzwa02@users.noreply.github.com>
2022-06-09 19:08:34 +0200

    Error handling

--- Cpp_17_Materials/Vector.cpp
+++ Cpp_17_Materials/Vector.cpp
@@ -8,10 +8,49 @@ Vector::Vector(int s)
 
 double& Vector::operator[](int i)
 {
+	if (i < 0 || size() <= i)
+		throw std::out_of_range{ "Vector::operator[]" };
 	return elem[i];
 }
 
 int Vector::size()
 {
 	return sz;
+}
+
+double read_and_sum(int s)
+{
+	Vector v(s);
+	for (int i = 0; i != v.size(); i++)
+		std::cin >> v[i];
+
+	double sum = 0;
+	for (int i = 0; i != v.size(); i++)
+		sum += v[i];
+
+	return sum;
+}
+
+Traffic_light& operator++(Traffic_light& t)
+{
+	switch (t)
+	{
+	case Traffic_light::green: return t = Traffic_light::yellow;
+	case Traffic_light::yellow: return t = Traffic_light::red;
+	case Traffic_light::red: return t = Traffic_light::green;
+	}
+}
+
+void f(Vector& v)
+{
+	// ...
+	try
+	{
+		v[v.size()] = 7;
+	}
+	catch (std::out_of_range& err)
+	{
+		std::cerr << err.what() << '\n';
+	}
+	// ...
 }
--- Cpp_17_Materials/Vector.h
+++ Cpp_17_Materials/Vector.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
+#include <numeric>
 
 enum class Color { red, blue, green };
 enum class Traffic_light { green, yellow, red };
@@ -15,3 +17,16 @@ public:
 	double& operator[](int);
 	int size();
 };
+
+double read_and_sum(int);
+
+Traffic_light& operator++(Traffic_light&);
+
+void f(Vector& v);
+
+void user(int sz) noexcept
+{
+	Vector v(sz);
+	std::iota(&v[0], &v[sz], 1);
+	// ...
+}
